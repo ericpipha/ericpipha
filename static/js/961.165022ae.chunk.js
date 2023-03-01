a0f8d008a081cacbae07d6128e56b4c9d80d9c92
Pichon-Pharabod <eric@Andorite.local>
2023-03-01 04:14:16 +0100

    Updates

---
+++ static/js/961.165022ae.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebpage=self.webpackChunkwebpage||[]).push([[961],{961:function(e,n,c){c.r(n),c.d(n,{ContactMeHref:function(){return t}});c(791);var u=c(184),t=function(e){return(0,u.jsx)("a",{href:"mailto:eric.pichon@polytechnique.edu",children:"eric.pichon@polytechnique.edu"})};n.default=t}}]);
+//# sourceMappingURL=961.165022ae.chunk.js.map
